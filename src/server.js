c6a001b6fa033abbaab8e1e0386e19b89c36b5e7
RaynaRaven <carolmarjara@gmail.com>
2023-03-16 00:12:56 +0000

    implement sessions, config auth strategy

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import Hapi from "@hapi/hapi";
 import Vision from "@hapi/vision";
 import Handlebars from "handlebars";
 import path from "path";
+import Cookie from "@hapi/cookie";
 
 // import utilities to locate folder paths
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,6 +20,7 @@ async function init() {
     host: "localhost",
   });
   await server.register(Vision);
+  await server.register(Cookie);
   server.views({
     engines: {
       hbs: Handlebars,
@@ -29,6 +32,16 @@ async function init() {
     layout: true,
     isCached: false,
   });
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: "myPOI",
+      password: "secretpasswordnotrevealedtoanyone",
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
   db.init();
   server.route(webRoutes);
   await server.start();
