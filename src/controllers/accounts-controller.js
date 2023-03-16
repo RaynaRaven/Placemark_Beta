6164a5bfa6a006afefa196e9b712860582770f5a
RaynaRaven <carolmarjara@gmail.com>
2023-03-16 01:07:49 +0000

    disable strategy for accounts routes

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -7,11 +7,13 @@ export const accountsController = {
     },
   },
   showSignup: {
+    auth: false,
     handler: function (request, h) {
-      return h.view("signup-view", { title: "Sign up for PlaceMark" });
+      return h.view("signup-view", { title: "Sign up for myPoi" });
     },
   },
   signup: {
+    auth: false,
     handler: async function (request, h) {
       const user = request.payload;
       await db.userStore.addUser(user);
@@ -19,23 +21,34 @@ export const accountsController = {
     },
   },
   showLogin: {
+    auth: false,
     handler: function (request, h) {
-      return h.view("login-view", { title: "Login to PlaceMark" });
+      return h.view("login-view", { title: "Login to myPOI" });
     },
   },
   login: {
+    auth: false,
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
       if (!user || user.password !== password) {
         return h.redirect("/");
       }
+      request.cookieAuth.set({ id: user._id});
       return h.redirect("/dashboard");
     },
   },
   logout: {
+    auth: false,
     handler: function (request, h) {
       return h.redirect("/");
     },
+  },
+  async validate(request, session) {
+    const user = await db.userStore.getUserById(session.id);
+    if (!user) {
+      return { isValid: false };
+    }
+    return { isValid: true, credentials: user };
   },
 };
