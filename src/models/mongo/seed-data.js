3965e86a2a6a0bea6b0b21b23889b897f374f59e
RaynaRaven <carolmarjara@gmail.com>
2023-03-19 00:41:20 +0000

    add mongodb seed data

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,39 @@
+export const seedData = {
+  users: {
+    _model: "User",
+    homer: {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret"
+    },
+    marge: {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret"
+    },
+    bart: {
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret"
+    }
+  },
+  categories: {
+    _model: "Category",
+    mozart: {
+      title: "Cheap Eats",
+      userid: "->users.bart"
+    }
+  },
+  locations: {
+    _model : "Location",
+    track_1 : {
+      name: "Billy Bunters",
+      description: "best fish box in town",
+      location: "52.83589902444309, -6.915504901522804",
+      categoryId: "->categories.cheap_eats"
+    },
+  }
+};
